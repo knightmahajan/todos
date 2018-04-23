8a171db9924524d3eafbd887afe271a20d039f6f
knightmahajan <kvmknight@gmail.com>
2018-04-23 17:06:28 +0530

    go

---
+++ src/todo/ToDo.java
@@ -0,0 +1,32 @@
+package todo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author computer
+ */
+public class ToDo extends Application implements  FXMLConst{
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(TODO_XML));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
